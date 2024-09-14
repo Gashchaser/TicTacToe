2a1ad45ba29a21eac9760c0fd14226b0f38c9693
Gashchaser <gregory.xzero@gmail.com>
2024-09-14 15:50:36 +0400

    Update Field.java

--- TicTacToe/src/Field.java
+++ TicTacToe/src/Field.java
@@ -1,6 +1,39 @@
-public class GameRunner {
-    public static void main(String[] args) {
-    GameLogic Start = new GameLogic();
-    Start.Start();
+public class Field {
+
+  public static char [][] map;
+
+    public char EMPTY_FIELD = '*';
+
+    public final int MAP_SIZE = 3;
+
+
+
+    public void printMap(){
+        // 0 1 2 3
+        // 1 * * *
+        // 2 * * *
+        // 3 * * *
+        for(int i = 0; i <= MAP_SIZE; i++){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for(int i=0; i < MAP_SIZE; i++){
+            System.out.print((i+1) + " ");
+            for(int j = 0; j<MAP_SIZE; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    public void Init() {
+        map = new char[MAP_SIZE][MAP_SIZE];
+        for(int i = 0; i < MAP_SIZE; i++){
+            for(int j = 0; j < MAP_SIZE; j++){
+                map[i][j] = EMPTY_FIELD;
+            }
+        }
+
     }
 }
+
